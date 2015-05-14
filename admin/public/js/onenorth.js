b17d907e3d57db0991138cc30fbf481595aa39d3
askesian <cwigley@gmail.com>
2015-05-14 15:14:13 -0500

    Check location.path against sidebar link href for active link

--- admin/public/js/onenorth.js
+++ admin/public/js/onenorth.js
@@ -1,6 +1,15 @@
 jQuery(function($) {
-	$('#sidebar-toggle').click(function(e) {
+	var path = window.location.pathname;
+
+  $('#sidebar-toggle').click(function(e) {
 		e.preventDefault();
 		$('body').toggleClass("sidebar-collapse");
 	});
+
+  $('.sidebar-nav a').filter(function(idx, link) {
+    if (link.getAttribute('href') === path) {
+      link.classList.add('is-active');
+    }
+  });
+
 });
