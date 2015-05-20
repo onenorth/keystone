439ed8bf63bd93d5b1262424994a7a4d440160d2
Mike Skutta <mskutta@onenorth.com>
2015-05-20 10:10:09 -0500

    Enabled expand/collapse

--- admin/public/js/onenorth.js
+++ admin/public/js/onenorth.js
@@ -1,15 +1,18 @@
 jQuery(function($) {
 	var path = window.location.pathname;
 
-  $('#sidebar-toggle').click(function(e) {
+	$('#sidebar-toggle').click(function(e) {
 		e.preventDefault();
 		$('body').toggleClass("sidebar-collapse");
 	});
 
-  $('.sidebar-nav a').filter(function(idx, link) {
-    if (link.getAttribute('href') === path) {
-      link.classList.add('is-active');
-    }
-  });
-
+	$('.sidebar-nav a').filter(function(idx, link) {
+	if (link.getAttribute('href') === path) {
+	  link.classList.add('is-active');
+	}
+	});
+	
+	$('.nav-toggle').click(function(e) {
+		var parent = $(e.target).closest("li").toggleClass( "expand" );
+	})
 });
